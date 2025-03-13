83b5ef7f0eedc014fcb9296170e440c49ffa4263
yahyabrk <cihanogluuyahyaberk@gmail.com>
2025-03-13 10:26:24 +0000

    Add new file

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
-# change it to print your name
-
-print("Hello, I am Piotr Brudny")
+class Person:
+  def __init__(self, name, age, address):
+    self.name = name
+    self.age = age
+    self.address = address
+p1 = Person("Fatih", 120 , 'opaczewska 32')
+print(p1.name)
+print(p1.age)
+print(p1.address)
